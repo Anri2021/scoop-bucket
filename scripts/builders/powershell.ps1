36f706a1242391c9a7182ca090fa3af0dde19064
Anri2021 <anri.il.2019@gmail.com>
2026-09-25 20:12:19 +0300

    Update powershell.ps1

--- scripts/builders/powershell.ps1
+++ scripts/builders/powershell.ps1
@@ -1,10 +1,5 @@
 [CmdletBinding()]
 param($Plan, $SourceRoot, $PackageDir, $CacheRoot)
 
-Push-Location $SourceRoot
-try {
-  $entry = [string](Get-Prop $Plan.recipe "entrypoint" (Get-Prop $Plan.recipe "bin"))
-  Copy-Item $entry $PackageDir -Force
-} finally {
-  Pop-Location
-}
+$entry = [string](Get-Prop $Plan.recipe "entrypoint" (Get-Prop $Plan.recipe "bin"))
+Copy-Item $entry $PackageDir -Force

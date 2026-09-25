6c2b6694d458007e0b66b75aa93e121c14eb7ef0
Anri2021 <anri.il.2019@gmail.com>
2026-09-25 20:13:29 +0300

    Update rust.ps1

--- scripts/builders/rust.ps1
+++ scripts/builders/rust.ps1
@@ -1,10 +1,5 @@
 [CmdletBinding()]
 param($Plan, $SourceRoot, $PackageDir, $CacheRoot)
 
-Push-Location $SourceRoot
-try {
-  Invoke-Checked "cargo" @("build", "--locked", "--release")
-  Get-ChildItem "target/release/*.exe" -File | Copy-Item -Destination $PackageDir -Force
-} finally {
-  Pop-Location
-}
+Invoke-Checked "cargo" @("build", "--locked", "--release")
+Get-ChildItem "target/release/*.exe" -File | Copy-Item -Destination $PackageDir -Force

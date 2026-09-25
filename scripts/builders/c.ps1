9467708f231df6df8b86dd8fce80c98a16418434
Anri2021 <anri.il.2019@gmail.com>
2026-09-25 18:46:00 +0300

    Update c.ps1

--- scripts/builders/c.ps1
+++ scripts/builders/c.ps1
@@ -6,8 +6,10 @@ if (-not (Test-Path $bash)) { throw "MSYS2 bash was not found." }
 $buildScript = @'
 set -e
 export PATH="/mingw64/bin:/usr/bin:$PATH"
-pacman -S --noconfirm --needed mingw-w64-x86_64-gcc mingw-w64-x86_64-glib2 autoconf automake libtool bison flex make
-autoreconf -fi
+export ACLOCAL_PATH="/mingw64/share/aclocal:/usr/share/aclocal"
+export PKG_CONFIG_PATH="/mingw64/lib/pkgconfig"
+pacman -S --noconfirm --needed mingw-w64-x86_64-gcc mingw-w64-x86_64-glib2 mingw-w64-x86_64-pkgconf pkgconf autoconf automake libtool bison flex make
+autoreconf -fi -I /mingw64/share/aclocal
 ./configure --prefix=/mingw64 --disable-man
 make -j$(nproc)
 '@ -replace "`r`n", "`n"
@@ -15,8 +17,8 @@ make -j$(nproc)
 Set-Content (Join-Path $SourceRoot "build.sh") -Value $buildScript -Encoding ascii
 Invoke-Checked $bash @("-lc", ("cd '{0}' && ./build.sh" -f ($SourceRoot -replace '\\', '/')))
 
-Get-ChildItem (Join-Path $SourceRoot "src/util/.libs/*.exe") | Copy-Item -Destination $PackageDir -Force
-Get-ChildItem (Join-Path $SourceRoot "src/libmdb/.libs/*.dll") | Copy-Item -Destination $PackageDir -Force
+Get-ChildItem (Join-Path $SourceRoot "src") -Filter "*.exe" -Recurse -File | Copy-Item -Destination $PackageDir -Force
+Get-ChildItem (Join-Path $SourceRoot "src") -Filter "*.dll" -Recurse -File | Copy-Item -Destination $PackageDir -Force
 $mingwDlls = @("libglib-2.0-0.dll", "libintl-8.dll", "libiconv-2.dll", "libpcre2-8-0.dll", "libwinpthread-1.dll")
 foreach ($dll in $mingwDlls) {
   $dllPath = "C:\msys64\mingw64\bin\$dll"
